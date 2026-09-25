5c5e9cbe070ea8143cb4c23c527c969264e47a69
Merlinna Line <7merlinna7@gmail.com>
2026-09-26 06:33:19 +0700

    0.1

---
+++ Assets/_Project/Develop/Runtime/Gameplay/GamemodeFeature/Gamemode.cs
@@ -0,0 +1,51 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.SequenceService;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.GamemodeFeature
+{
+    public class Gamemode: MonoBehaviour
+    {
+        private int _sequenceLenth = 5;
+        private int _sequenceCount = 0;
+        private Sequence _sequence;
+        private Dictionary<KeyCode, char> _sequencePool = new Dictionary<KeyCode, char>()
+        {
+            { KeyCode.Alpha0,'0' },
+            { KeyCode.Alpha1,'1' },
+            { KeyCode.Alpha2,'2' },
+            { KeyCode.Alpha3,'3' },
+            { KeyCode.Alpha4,'4' },
+            { KeyCode.Alpha5,'5' },
+            { KeyCode.Alpha6,'6' },
+            { KeyCode.Alpha7,'7' },
+            { KeyCode.Alpha8,'8' },
+            { KeyCode.Alpha9,'9' },
+        };
+
+        public void Start()
+        {
+            _sequence = new Sequence(_sequencePool, _sequenceLenth);
+            _sequence.Create();
+
+            Debug.Log(_sequence.Get());
+        }
+
+        public void Update()
+        {
+            if (Input.anyKeyDown == true)
+            {
+                if (Input.GetKeyDown(_sequence.GetNextValueKeyCode()))
+                {
+                    Debug.Log("Good");
+                    _sequenceCount++;
+                }
+                else
+                    Debug.Log("Defeat");
+            }
+
+            if (_sequenceCount == _sequenceLenth)
+                Debug.Log("Win");
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Gameplay/SequenceService/Sequence.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.SequenceService
+{
+    public class Sequence
+    {
+        private Dictionary<KeyCode,char> _sequencePool;
+
+        private List<char> _sequenceValues;
+
+        private int _sequenceLenth;
+
+        private Queue<char> _currentSequence;
+
+        public Sequence(Dictionary<KeyCode,char> sequencePool, int sequenceLenth)
+        {
+            _sequencePool = sequencePool;
+            _sequenceLenth = sequenceLenth;
+
+            _currentSequence = new();
+        }
+
+        public void Create()
+        {
+            if (_sequenceValues == null)
+                SetValues();
+
+            Reset();
+
+            for (int i = 0; i < _sequenceLenth; i++)
+                AddRandomValueToSequence();
+        }
+
+        public KeyCode GetNextValueKeyCode()
+        {
+            if (_currentSequence == null)
+                throw new NullReferenceException("Sequense is null, please create the sequence before using");
+
+            char value = _currentSequence.Dequeue();
+            KeyCode valueKeyCode = _sequencePool.First(kvp => kvp.Value.Equals(value)).Key;
+
+            return valueKeyCode;
+        }
+
+        public string Get()
+        {
+            string sequence = "";
+            for (int i = 0; i < _sequenceLenth; i++)
+            {
+                char value = _currentSequence.Dequeue();
+                sequence += value;
+                _currentSequence.Enqueue(value);
+            }
+
+            return sequence;
+        }
+
+        private void Reset() => _currentSequence.Clear();
+
+        private void SetValues()
+        {
+            _sequenceValues = new();
+
+            foreach (var kvp in _sequencePool)
+                _sequenceValues.Add(kvp.Value);
+        }
+
+        private void AddRandomValueToSequence()
+        {
+            char randomValue = _sequenceValues[Random.Range(0, _sequenceValues.Count)];
+            _currentSequence.Enqueue(randomValue);
+        }
+    }
+}
